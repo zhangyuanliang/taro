a7bd8f2dd6bf57295b60ddf6fa0ba7f45adaa01e
zhangyuanliang <805013595@qq.coml>
2023-08-09 13:23:37 +0800

    feat: 内存泄漏测试代码

--- examples/custom-tabbar-vue3/src/pages/index/index.vue
+++ examples/custom-tabbar-vue3/src/pages/index/index.vue
@@ -1,7 +1,7 @@
 <template>
   <view class="index">
-    <text>我是首页！</text>
-    <text @tap="toPage">ToPage</text>
+    <text>我是首页！多次点击：</text>
+    <text @tap="toPage" class="blue">ToPage</text>
   </view>
 </template>
 
@@ -13,3 +13,9 @@ const toPage = () => {
   })
 }
 </script>
+
+<style>
+.blue {
+  color: rgb(80, 80, 231);
+}
+</style>
--- examples/custom-tabbar-vue3/src/pages/sub/index.vue
+++ examples/custom-tabbar-vue3/src/pages/sub/index.vue
@@ -4,7 +4,49 @@
 
 <template>
   <view>
-    <text>sub/test</text>
+    <view>
+      <text>点击上方返回</text>
+    </view>
+    <image src="/images/tabbar_cart_on.png"></image>
+    <image src="/images/tabbar_cart.png"></image>
+    <image src="/images/tabbar_cate_on.png"></image>
+    <image src="/images/tabbar_cate.png"></image>
+    <image src="/images/tabbar_home_on.png"></image>
+    <image src="/images/tabbar_home.png"></image>
+    <image src="/images/tabbar_my_on.png"></image>
+    <image src="/images/tabbar_my.png"></image>
+    <image src="/images/tabbar_cart_on.png"></image>
+    <image src="/images/tabbar_cart.png"></image>
+    <image src="/images/tabbar_cate_on.png"></image>
+    <image src="/images/tabbar_cate.png"></image>
+    <image src="/images/tabbar_home_on.png"></image>
+    <image src="/images/tabbar_home.png"></image>
+    <image src="/images/tabbar_my_on.png"></image>
+    <image src="/images/tabbar_my.png"></image>
+    <image src="/images/tabbar_cart_on.png"></image>
+    <image src="/images/tabbar_cart.png"></image>
+    <image src="/images/tabbar_cate_on.png"></image>
+    <image src="/images/tabbar_cate.png"></image>
+    <image src="/images/tabbar_home_on.png"></image>
+    <image src="/images/tabbar_home.png"></image>
+    <image src="/images/tabbar_my_on.png"></image>
+    <image src="/images/tabbar_my.png"></image>
+    <image src="/images/tabbar_cart_on.png"></image>
+    <image src="/images/tabbar_cart.png"></image>
+    <image src="/images/tabbar_cate_on.png"></image>
+    <image src="/images/tabbar_cate.png"></image>
+    <image src="/images/tabbar_home_on.png"></image>
+    <image src="/images/tabbar_home.png"></image>
+    <image src="/images/tabbar_my_on.png"></image>
+    <image src="/images/tabbar_my.png"></image>
+    <image src="/images/tabbar_cart_on.png"></image>
+    <image src="/images/tabbar_cart.png"></image>
+    <image src="/images/tabbar_cate_on.png"></image>
+    <image src="/images/tabbar_cate.png"></image>
+    <image src="/images/tabbar_home_on.png"></image>
+    <image src="/images/tabbar_home.png"></image>
+    <image src="/images/tabbar_my_on.png"></image>
+    <image src="/images/tabbar_my.png"></image>
     <image src="/images/tabbar_cart_on.png"></image>
     <image src="/images/tabbar_cart.png"></image>
     <image src="/images/tabbar_cate_on.png"></image>
